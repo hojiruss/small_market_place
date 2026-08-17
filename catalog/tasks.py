72421bc416cab81b22b120d5b408e86fe2e881f8
HojiRuss <hojat96shoapour@gmail.com>
2026-08-17 13:32:39 +0330

    fix views for cache things and invalidations

--- catalog/tasks.py
+++ catalog/tasks.py
@@ -6,8 +6,3 @@ from django.core.cache import cache
 def notify_new_product(product_name):
     print(f'new product {product_name} came!!')
     return "Done"
-
-@shared_task
-def invalidate_product_cache_list(product_name):
-    cache.delete(product_name)
-    return "Done"
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,11 @@
-from django.utils.decorators import method_decorator
 from rest_framework import viewsets
 from catalog.permissions import IsOwnerOrReadOnly
 from .models import Product
 from catalog.serializers import ProductSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-from catalog.tasks import notify_new_product,invalidate_product_cache_list
-from django.views.decorators.cache import cache_page
+from catalog.tasks import notify_new_product
+from django.core.cache import cache
+from rest_framework.response import Response
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
@@ -13,14 +13,29 @@ class ProductViewSet(viewsets.ModelViewSet):
     permission_classes = [IsOwnerOrReadOnly]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['seller']
-    @method_decorator(cache_page(60*2))
-    def list(self,request):
-        queryset = Product.objects.all()
-        serializer = ProductSerializer(queryset, many=True)
+
+    def list(self,request,*args,**kwargs):
+        cache_key = f'product_list:{request.get_full_path()}'
+        cached_data = cache.get(cache_key)
+        if cached_data is not None:
+            return Response(cached_data)
+        response = super().list(request,*args,**kwargs)
+        cache.set(cache_key,response.data,timeout=60*5)
+        return response
 
     def perform_create(self, serializer):
         instance = serializer.save(seller=self.request.user)
         notify_new_product.delay(instance.name)
-        invalidate_product_cache_list.delay(instance.name)
+        cache.delete_pattern('product_list:*')
+
+
+    def perform_update(self,serializer):
+        serializer.save()
+        cache.delete_pattern('product_list:*')
+
+
+    def perform_destroy(self,instance):
+        instance.delete()
+        cache.delete_pattern('product_list:*')
 
 
